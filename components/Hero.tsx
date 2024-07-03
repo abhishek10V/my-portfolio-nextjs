e50bd97ab59b5686d32a1393ff442d66dd96715e
Abhishek Varshney <mnra1529@gmail.com>
2024-07-03 15:36:37 +0530

    Hero Section is Updated and Spotlights are added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="pb-20 pt-36">
+      <div>
+       <Spotlight className='-top-40 -left-10 md:-top-20 md:-left-32 h-screen' fill='white'/>
+       <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='white'/>
+       <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='white'/>
+      </div>
+
+      <div>
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        Backgrounds
+      </p>
+    </div>
+      </div>
+    </div>
   )
 }
 
